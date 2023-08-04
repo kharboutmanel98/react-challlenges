8e6ccb125954236f586ec861e3328d45b73e9e7c
kharboutmanel98 <kharbout2017@gmail.com>
2023-08-04 13:45:41 +0200

    updating states several times challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
-import { useState } from 'react';
 import './App.css';
-import ShoppingCart from './ShoppingCart';
+import Counter from './Counter';
 
 function App() {
-
+  // const [count, setCount] =useState(0)
+  // function handlePlusClick(){
+  //   setCount((c)=> {
+      
+  //     return c + 1;
+  //   });
+     
+    
+  //    setCount(count + 1)
+  //   } 
+    
+  
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ShoppingCart /> */}
+        {/* <h1>this count is: {count}</h1>
+        <button onClick={handlePlusClick}>+</button> */}
+        <Counter />
       </header>
     </div>
   );
